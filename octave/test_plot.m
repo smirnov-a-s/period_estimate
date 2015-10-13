7ee16e25ef8ec802afb76c63fc26f7724f9cfd27
Anton Smirnov <asmirnov@stc-spb.ru>
2015-10-13 14:58:34 +0300

    testing

--- octave/test_plot.m
+++ octave/test_plot.m
@@ -1,33 +1,38 @@
 #!/usr/bin/octave -qf
 
-x = load("./dump/nccf.dat");
+fid = fopen("./dump/main_input_norm_float_bin.dat","r");
+x = fread(fid, Inf, "float32", 0);
+fclose(fid);
+
 len = length(x)
-
-plot(x);
-
-## fid = fopen("./dump/sig_bin.dump","r");
-## x = fread(fid, Inf, "int32", 0);
-## fclose(fid);
-
-## len = length(x)
 ## figure(1)
 ## plot(x)
 ## grid on;
 
-## Fs = 200;
-## N = 256;
-## spect = fft(x, N);
-## spect = spect / N;
-## spect = abs(spect);
+corr_win = 256;
+max_lag = 1024;
 
-## ##
-## f_axis = Fs/2 * linspace(0, 1, N/2 + 1);
+w = x(1:1024);
 
-## printf("f_axis len = %d\n", length(f_axis));
-## printf("delta f    = %f\n", Fs / N);
+res = [];
+stop = length(x);
+i = 1;
+while (i < stop - corr_win)
+  win = x(i : i + corr_win - 1)';
+  length(win);
+  res = [res, win];
+  i = i + corr_win;
+endwhile
 
-## figure(2);
-## plot(f_axis, spect(1 : length(f_axis)));
-## grid on;
+## cc = normxcorr2(x, w);
 
-input("");
+## [corr, lag] = xcorr(x, max_lag);
+
+## plot(corr)
+## plot(lag)
+
+## plot(res);
+## hold on;
+## plot(x, 'r');
+
+## input("");
